0b82f7fa8ab04910385e6028dc3380baf84d4d36
Michael Jones <michael.jones90@me.com>
2026-06-20 14:18:34 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-CupcakeCorner/HackingWithSwiftUI_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_CupcakeCornerApp.swift
+//  HackingWithSwiftUI-CupcakeCorner
+//
+//  Created by Michael Jones on 20/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
