ef9cbdb9eab0f786e2d4b120c2f4e51b7702661b
Michael Jones <michael.jones90@me.com>
2026-07-15 14:05:20 +0100

    refactoring ContentView so it conforms to the MVVM model.

--- HackingWithSwiftUI-CupcakeCorner/View/ContentView.swift
+++ HackingWithSwiftUI-CupcakeCorner/View/ContentView.swift
@@ -8,33 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var order = Order()
+//    @State private var order = Order()
+    @State private var viewModel = ViewModel()
     
     var body: some View {
         NavigationStack {
             Form {
                 Section() {
-                    Picker("Select your cake topping?", selection: $order.type) {
+                    Picker("Select your cake topping?", selection: $viewModel.order.type) {
                         ForEach(Order.types.indices, id: \.self) {
                             Text(Order.types[$0])
                         }
                     }
                     
-                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                    Stepper("Number of cakes: \(viewModel.order.quantity)", value: $viewModel.order.quantity, in: 3...20)
                 }
                 
                 Section() {
-                    Toggle("Any Special Requests?", isOn: $order.specialRequestEnabled.animation())
+                    Toggle("Any Special Requests?", isOn: $viewModel.order.specialRequestEnabled.animation())
                     
-                    if order.specialRequestEnabled {
-                        Toggle("Extra Frosting?", isOn: $order.extraFrosting)
-                        Toggle("Add Sprinkles?", isOn: $order.addSprinkles)
+                    if viewModel.order.specialRequestEnabled {
+                        Toggle("Extra Frosting?", isOn: $viewModel.order.extraFrosting)
+                        Toggle("Add Sprinkles?", isOn: $viewModel.order.addSprinkles)
                     }
                 }
                 
                 Section() {
                     NavigationLink("Delivery Details") {
-                        AddressView(order: order)
+                        AddressView(order: viewModel.order)
                     }
                 }
 
---
+++ HackingWithSwiftUI-CupcakeCorner/ViewModel/ViewModel-ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModel-ContentView.swift
+//  HackingWithSwiftUI-CupcakeCorner
+//
+//  Created by Michael Jones on 15/07/2026.
+//
+
+import Foundation
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        var order = Order()
+    }
+}
